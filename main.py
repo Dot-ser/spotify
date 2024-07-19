0d462371ab621220d751d2836f1a63348e2a196a
Alosious Benny <105029178+Dot-ser@users.noreply.github.com>
2024-07-19 14:46:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
-app = Flask(name)
+app = Flask(__name__)
 
-@app.route('/hi', methods=['GET'])
-def say_hi():
-return jsonify({'message': 'Hi'})
+@app.route('/reply', methods=['GET', 'POST'])
+def reply():
+    if request.method == 'POST':
+        data = request.get_json()
+        message = data.get('message', '')
+        return jsonify({'reply': f'You said: {message}'})
+    else:
+        return jsonify({'reply': 'Send a POST request with a message to get a reply'})
 
-if name == 'main':
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
